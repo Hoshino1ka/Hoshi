543063d7ef0200f05ecf607daddcfd3bcca67ce8
Hoshino1ka <mc232d5y0804@student.devacademy.id>
2025-03-10 21:36:29 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -24,31 +24,39 @@ plot_type = st.radio("Select plot type:", ("Season", "Workday"))
 fig, ax = plt.subplots()
 if plot_type == "Season":
     plt.figure(figsize=(10, 5))
-colors = ["#D3D3D3", "#D3D3D3", "#72BCD4", "#D3D3D3"]
-data1 = hour.groupby(by='season').cnt.mean().sort_values(ascending=False).reset_index()
-sns.barplot(
-    y="cnt",
-    x="season",
-    data=data1,
-    palette=colors
-)
-plt.title("Number of Rental", loc="center", fontsize=15)
-plt.ylabel(None)
-plt.xlabel(None)
-plt.xticks(ticks=[0, 1, 2, 3], labels=['Springger', 'Summer', 'Fall', 'Winter'])
+    colors = ["#D3D3D3", "#D3D3D3", "#72BCD4", "#D3D3D3"]
+    data1 = hour.groupby(by='season').cnt.mean().sort_values(ascending=False).reset_index()
+    sns.barplot(
+        y="cnt",
+        x="season",
+        data=data1,
+        palette=colors
+    )
+    plt.title("Number of Rental", loc="center", fontsize=15)
+    plt.ylabel(None)
+    plt.xlabel(None)
+    plt.xticks(ticks=[0, 1, 2, 3], labels=['Springger', 'Summer', 'Fall', 'Winter'])
+    st.pyplot(plt)
+
 elif plot_type == "Workday":
-   plt.figure(figsize=(10, 5))
-colors = ["#D3D3D3", "#72BCD4"]
-data1 = hour.groupby(by='workingday').cnt.mean().sort_values(ascending=False).reset_index()
-sns.barplot(
-    y="cnt",
-    x="workingday",
-    data=data1,
-    palette=colors
-)
-plt.title("Number of Rental", loc="center", fontsize=15)
-plt.ylabel(None)
-plt.xlabel(None)
-plt.xticks(ticks=[0, 1], labels=['Holiday', 'Workday'])
+    plt.figure(figsize=(10, 5))
+    colors = ["#D3D3D3", "#72BCD4"]
+    
+    # Ensure 'workingday' column exists and contains valid values
+    if 'workingday' in hour.columns and hour['workingday'].notna().all():
+        data1 = hour.groupby(by='workingday').cnt.mean().sort_values(ascending=False).reset_index()
+        sns.barplot(
+            y="cnt",
+            x="workingday",
+            data=data1,
+            palette=colors
+        )
+        plt.title("Number of Rental", loc="center", fontsize=15)
+        plt.ylabel(None)
+        plt.xlabel(None)
+        plt.xticks(ticks=[0, 1], labels=['Holiday', 'Workday'])
+        st.pyplot(plt)
+    else:
+        st.error("Column 'workingday' contains invalid or missing values.")
 
 st.pyplot(fig)
